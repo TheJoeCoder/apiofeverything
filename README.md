260319ca0a1a4deffb24394f738fb11bf82951a2
TheJoeCoder <joeleaver77@gmail.com>
2020-08-13 17:07:20 +0100

    Added stuff

---
+++ README.md
@@ -0,0 +1,72 @@
+# API Of Everything
+
+APIOfEverything is a web API with a few things that could possibly be useful in some code.
+
+## How to install
+
+When I've set up a site with the API on you won't necessarily need to install this yourself but in the meantime:
+
+1. Clone the repository
+
+```
+git clone https://github.com/TheJoeCoder/apiofeverything
+
+cd apiofeverything
+```
+
+2. Install the packages
+
+```
+npm install
+```
+
+3. Launch the web server
+
+```
+npm start
+```
+
+
+## How it works (for developers)
+
+If you want to use this in your project, feel free to. Here's a guide.
+
+1. Make a web request to the apiofeverything server.
+
+2. Check if the response is an error by seeing if the JSON output's error is false
+
+3. Do stuff with the other output.
+
+### Example Output from the apiofeverything server
+
+#### Example output of /time/now
+
+```json
+{
+  "error": false,
+  "errormessage": null,
+  "timezone": "ETC/UTC",
+  "datestring":"2020-08-13",
+  "day":"13",
+  "dayname":"thursday",
+  "dayofyear":226,
+  "daytime":"afternoon",
+  "month":"08",
+  "monthname":"august",
+  "year":"2020",
+  "hour":15,
+  "minute":39,
+  "second":5,
+  "time24h":"15:39",
+  "time12h":"3:39pm"
+}
+```
+
+#### Example output of an incorrect timezone (/time/L/now)
+
+```json
+{
+  "error": true,
+  "errormessage": "IncorrectTimezone",
+}
+```
--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const informal = require('spacetime-informal');
 const express = require('express');
 const app = express();
 
-function timeNow(zone) {
+var configpath = './config.json';
+var config = require(configpath);
+function reloadConfig() {
+  config = require(configpath);
+}
+
+var serverport = config[port] || 3000;
+
+app.get('/time/now', (req, res) => {
+  var reqbody = timeNow("Etc/UTC");
+  return res.send(reqbody);
+});
+app.get('/time/:timezone/now', (req, res) => {
+  var zone = req.params.timezone;
   var time = informal.find(zone);
   var reqbody = {};
   if(time == null) {
@@ -18,17 +31,29 @@ function timeNow(zone) {
       errormessage: null,
       timezone: time,
       datestring: d.unixFmt('yyyy-MM-dd'),
-      day: d.unixFmt('dd'),
+      day: d.date(),
+      month: d.month(),
+      year: d.tear(),
+      hour: d.hour(),
+      minute: d.minute(),
+      second: d.second(),
+      ampm: d.ampm(),
+      time24h: `${d.hour()}:${d.minute()}`,
+      time12h: d.time(),
       dayname: d.dayName(),
       dayofyear: d.dayOfYear(),
-      month: d.unixFmt('MM'),
+      daytime: d.dayTime(),
       monthname: d.monthName(),
-      year: d.unixFmt('yyyy')
+      quarter: d.quarter()
     };
   }
-  return reqbody;
-}
-function timeOnDate(zone, year, month, day) {
+  return res.send(reqbody);
+});
+app.get('/time/:timezone/bydate/:year/:month/:day', (req, res) => {
+  var zone = req.params.timezone;
+  var year = req.params.year;
+  var month = req.params.month;
+  var date = req.params.day;
   var time = informal.find(zone);
   var reqbody = {};
   if(time == null) {
@@ -36,21 +61,32 @@ function timeOnDate(zone, year, month, day) {
       error: true,
       errormessage: 'IncorrectTimezone'
     };
-  } else {}
-}
-
-app.get('/time/now', (req, res) => {
-  var reqbody = timeNow("Etc/UTC");
+  } else {
+    var d = spacetime([year, month, day], time);
+    reqbody = {
+      error: false,
+      errormessage: null,
+      timezone: time,
+      datestring: d.unixFmt('yyyy-MM-dd'),
+      day: d.date(),
+      month: d.month(),
+      year: d.tear(),
+      hour: d.hour(),
+      minute: d.minute(),
+      second: d.second(),
+      ampm: d.ampm(),
+      time24h: `${d.hour()}:${d.minute()}`,
+      time12h: d.time(),
+      dayname: d.dayName(),
+      dayofyear: d.dayOfYear(),
+      daytime: d.dayTime(),
+      monthname: d.monthName(),
+      quarter: d.quarter()
+    };
+  }
   return res.send(reqbody);
 });
-app.get('/time/:timezone/now', (req, res) => {
-  var reqbody = timeNow(req.params.timezone);
-  return res.send(reqbody);
-});
-app.get('/time/:timezone/:year/:month/:date', (req, res) => {
 
-});
-
-app.listen(3000, () =>
-  console.log(`Example app listening on port 3000!`),
+app.listen(serverport, () =>
+  console.log(`App listening on port ${serverport}!`),
 );
